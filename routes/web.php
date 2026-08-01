e8b13e5e27d294801fca14598affa53f82e9c3f1
faizrhidayat <faizrajmat.hidayat06@gmail.com>
2026-08-01 23:38:20 +0700

    refactor: organize routes into per-module groups (branches, users)

--- routes/web.php
+++ routes/web.php
@@ -21,26 +21,30 @@ Route::middleware(['auth'])->group(function () {
     Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-    // Route names registered ahead of their controllers (Task 2/3 of the
-    // Administrasi CRUD UI plan) so the sidebar's route() calls resolve.
-    // Route::get() only needs the controller class to exist once the route
-    // is actually dispatched, not at registration/URL-generation time.
-    Route::get('/branches', [BranchController::class, 'index'])->name('branches.index');
-    Route::get('/branches/create', [BranchController::class, 'create'])->name('branches.create');
-    Route::post('/branches', [BranchController::class, 'store'])->name('branches.store');
-    Route::get('/branches/{branch}/edit', [BranchController::class, 'edit'])->name('branches.edit');
-    Route::put('/branches/{branch}', [BranchController::class, 'update'])->name('branches.update');
+    Route::prefix('branches')->name('branches.')->group(function () {
+        Route::get('/', [BranchController::class, 'index'])->name('index');
+        Route::get('/create', [BranchController::class, 'create'])->name('create');
+        Route::post('/', [BranchController::class, 'store'])->name('store');
+        Route::get('/{branch}/edit', [BranchController::class, 'edit'])->name('edit');
+        Route::put('/{branch}', [BranchController::class, 'update'])->name('update');
+    });
 
-    Route::get('/users', [UserController::class, 'index'])->name('users.index');
-    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-    Route::post('/users', [UserController::class, 'store'])->name('users.store');
-    Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
-    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::prefix('users')->name('users.')->group(function () {
+        Route::get('/', [UserController::class, 'index'])->name('index');
+        Route::get('/create', [UserController::class, 'create'])->name('create');
+        Route::post('/', [UserController::class, 'store'])->name('store');
+        Route::get('/{user}', [UserController::class, 'show'])->name('show');
+        Route::put('/{user}', [UserController::class, 'update'])->name('update');
 
-    Route::post('/users/{user}/branches/{branch}', [UserBranchAssignmentController::class, 'store'])->name('users.branches.store');
-    Route::delete('/users/{user}/branches/{branch}', [UserBranchAssignmentController::class, 'destroy'])->name('users.branches.destroy');
-    Route::put('/users/{user}/branches/{branch}/default', [UserBranchAssignmentController::class, 'setDefault'])->name('users.branches.setDefault');
+        Route::prefix('{user}/branches')->name('branches.')->group(function () {
+            Route::post('/{branch}', [UserBranchAssignmentController::class, 'store'])->name('store');
+            Route::delete('/{branch}', [UserBranchAssignmentController::class, 'destroy'])->name('destroy');
+            Route::put('/{branch}/default', [UserBranchAssignmentController::class, 'setDefault'])->name('setDefault');
+        });
 
-    Route::post('/users/{user}/permissions/{permission}', [UserPermissionAssignmentController::class, 'store'])->name('users.permissions.store');
-    Route::delete('/users/{user}/permissions/{permission}', [UserPermissionAssignmentController::class, 'destroy'])->name('users.permissions.destroy');
+        Route::prefix('{user}/permissions')->name('permissions.')->group(function () {
+            Route::post('/{permission}', [UserPermissionAssignmentController::class, 'store'])->name('store');
+            Route::delete('/{permission}', [UserPermissionAssignmentController::class, 'destroy'])->name('destroy');
+        });
+    });
 });
